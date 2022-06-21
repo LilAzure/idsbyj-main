f0aaa1b3aa2fe940fb46d12b964fb68b4e1fe290
LilAzure <49159755+LilAzure@users.noreply.github.com>
2022-06-20 21:53:45 -0400

    commit error

--- src/assets/menuData.js
+++ src/assets/menuData.js
@@ -76,7 +76,6 @@ export const menuData = {
         { name: '', price: '' },
         { name: '', price: '' },
         { name: '', price: '' },
-        { name: '', price: '' },
     ],
 
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -35,14 +35,16 @@ const Header = () => {
 
 	return (
 		<div className='sticky top-0 bg-gray-50 z-10'>
-			<header className='container w-8/12 h-5/12 mx-auto mb-8 mt-6 px-4 py-2 flex justify-between items-end text-gray-900'>
+			<header className='container mx-auto mb-8 mt-6 px-4 py-2 flex justify-between items-center text-gray-900'>
 				<Link
 					to='/'
-					className='text-5xl font-display mr-8'
+					className='text-1xl font-display mr-8'
 					onClick={() => setTimeout(() => window.scrollTo(0, 0), 250)}
 					aria-label='Clicking this logo takes you to the top of the page'
 				>
-					<img src={logo} alt="header logo" />
+					{/* iDesigns Studio By J */}
+					<img class='object-contain h-32 w-full container'
+						src={logo} alt="header logo" />
 				</Link>
 				{showMenu()}
 				<MobileMenu setOpen={setOpen} isOpen={isOpen} />
